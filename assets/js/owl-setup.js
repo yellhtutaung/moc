369f84d4fcbb9a51ea6d21f4a596c5731f2d49f2
yellhtutaung <yellhtut4@gmail.com>
2023-06-07 11:26:00 +0630

    moc mobile responsive

---
+++ assets/js/owl-setup.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:5,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
